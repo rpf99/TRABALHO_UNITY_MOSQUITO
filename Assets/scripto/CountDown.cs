682093858909c016cd0e5709f764423d131112b1
rvbarross <rvbarross@gmail.com>
2022-11-19 13:45:20 -0300

    added spawner destroyer function

--- Assets/scripto/CountDown.cs
+++ Assets/scripto/CountDown.cs
@@ -1,29 +1,31 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CountDown : MonoBehaviour
-{
-    public Text minutesText;
-    public Text secondsText;
+public class CountDown : MonoBehaviour {
     
-    public int minutos;
-    public float segundos;
+    public Text timeText;
+    private int minutos;
+    private float segundos;
 
+    //Princiapl: 5 minutos, 0 segundos
+    private void Start() {
+        minutos = 0;
+        segundos = 10f;
+    }
+    
     void FixedUpdate() {
+        timeText.text = String.Format("{0:00}:{1:00}", this.minutos, this.segundos);
         
-        minutesText.text = minutos.ToString("00");
-        secondsText.text = segundos.ToString("00");
         if (segundos <= 0.01) {
-            if (minutos == 0)
-            {
-                UnityEditor.EditorApplication.isPlaying = false;
-            }
-            else
-            {
+            if (minutos == 0) {
+                timeText.text = "Game Over";
+                Time.timeScale = 0;
+            }else {
                 minutos -= 1;
                 segundos = 59;
             }
