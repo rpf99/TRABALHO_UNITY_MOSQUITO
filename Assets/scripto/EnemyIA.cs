5b0e20d4b68805f7c268ea279ab0b804743e64a2
Thiago Diniz <thiagodinizf@gmail.com>
2022-11-23 08:41:08 -0300

    add enemy animations player chasing

---
+++ Assets/scripto/EnemyIA.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIA : MonoBehaviour
+{
+    public float speed;
+    public float checkRadius;
+    public float attackRadius;
+
+    public bool shouldRotate;
+    
+    public LayerMask whatIsPlayer;
+
+    private Transform target;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Vector2 movement;
+    public Vector3 dir;
+
+    private bool isInChaseRange;
+    private bool isInAttackRange;
+
+    private void Start() 
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player").transform;
+
+    }
+    private void Update()
+    {
+        animator.SetBool("isFlying", isInChaseRange);
+        isInChaseRange = Physics2D.OverlapCircle(transform.position, checkRadius, whatIsPlayer);
+        isInAttackRange = Physics2D.OverlapCircle(transform.position, attackRadius, whatIsPlayer);
+
+        dir = target.position - transform.position;
+        dir.Normalize();
+        movement = dir;
+        if(shouldRotate)
+        {
+            animator.SetFloat("X", dir.x);
+            animator.SetFloat("Y", dir.y);
+        }
+    }
+    private void FixedUpdate()
+    {
+        if(isInChaseRange && isInAttackRange)
+        {
+            MoveCharacter(movement);
+        }
+        if(isInAttackRange)
+        {
+            rb.velocity = Vector2.zero;
+
+        }
+    }
+    private void MoveCharacter(Vector2 dir)
+    {
+        rb.MovePosition((Vector2)transform.position + (dir * speed * Time.deltaTime));
+    }
+}
