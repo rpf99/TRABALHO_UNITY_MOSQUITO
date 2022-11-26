1bc7630ed4e3ed82fc52544ecb066bd95b1c2643
rvbarross <rvbarross@gmail.com>
2022-11-25 22:19:48 -0300

    added life bar

---
+++ Assets/scripto/HealthController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    public float health = 3;
+    public float healthMax = 15;
+    public Image healthBar;
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthBar.fillAmount = health / healthMax;
+    }
+}
