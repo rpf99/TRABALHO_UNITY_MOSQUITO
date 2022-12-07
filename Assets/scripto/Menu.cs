32ff188903d294368fde7718f5ce58abb5e30733
rvbarross <rvbarross@gmail.com>
2022-12-07 07:41:14 -0300

    added menu

---
+++ Assets/scripto/Menu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevelDeJogo;
+
+    public void Jogar()
+    {
+        SceneManager.LoadScene(nomeDoLevelDeJogo);
+    }
+
+    public void SairJogo()
+    {
+        Application.Quit();
+    }
+}
